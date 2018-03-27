e7504643856b3dba858a2bc330ef96933dbf8393
XiaoV-Man <1819987625@qq.com>
2018-03-27 21:33:46 +0800

    提交代码

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("lalalala");
+        System.out.println("lalalala");
+        System.out.println("lalalala");
     }
 }
